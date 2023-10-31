fd6bf308e1d2c9206b16e482454fd20623bd89da
whps40333 <whps40333@gmail.com>
2023-10-31 18:46:25 +0800

    finish day_26 pratice

---
+++ script.js
@@ -0,0 +1,45 @@
+const triggers = document.querySelectorAll('.cool > li');
+// arrow container
+const background = document.querySelector('.dropdownBackground');
+// 取得指向框的位置
+const backgroundCoords = background.getBoundingClientRect();
+
+/**
+ * 移動到元素上的觸發事件
+ */
+function handleEnter() {
+    this.classList.add('trigger-enter');
+    background.classList.add('open');
+
+    // 取得下拉選單內容 DOM
+    const dropdown = this.querySelector('.dropdown');
+    // 取得位置資料
+    const dropCoords = dropdown.getBoundingClientRect();
+
+    // 設定指向框大小為下拉選單大小
+    background.style.width = `${dropCoords.width}px`;
+    background.style.height = `${dropCoords.height}px`;
+    // 使用 setProperty
+    // background.style.setProperty('width', `${dropCoords.width}px`);
+    // background.style.setProperty('height', `${dropCoords.height}px`);
+
+    // 移動指向框
+    background.style.transform = `translate(${dropCoords.left - backgroundCoords.left}px,${dropCoords.top - backgroundCoords.top}px)`;
+    // 使用 setProperty
+    // background.style.setProperty('transform', `translate(${dropCoords.left - backgroundCoords.left}px,${dropCoords.top - backgroundCoords.top}px)`);
+    // 調整內容最後顯示
+    this.classList.add('trigger-enter-active');
+}
+
+/**
+ * 離開元素的觸發事件
+ */
+function handleLeave() {
+    this.classList.remove('trigger-enter','trigger-enter-active');
+    background.classList.remove('open');
+}
+
+// 移動到元素上的觸發事件
+triggers.forEach(trigger => trigger.addEventListener('mouseenter',handleEnter));
+// 離開元素的觸發事件
+triggers.forEach(trigger => trigger.addEventListener('mouseleave',handleLeave));
